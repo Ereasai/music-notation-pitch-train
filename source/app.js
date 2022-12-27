2e0941a996a2c50bff07ecad12dc25aca48dcecc
Gunwoo Kim <gunwoolouie@gmail.com>
2022-12-26 20:48:02 -0800

    Added piano visualizer.

--- source/app.js
+++ source/app.js
@@ -2,34 +2,40 @@
 /* VexFlow */
 const { Renderer, Stave, StaveNote, Voice, Formatter, Accidental, Beam } = Vex.Flow;
 
+/* CONSTANTS */
 const STAVE_LENGTH      = 80;
 const STAVE_XOFFSET     = 10;
 const STAVE_BASS_XSTART = 80;
-
 const MIDDLE_C_ABS      = 39;
 
-
+/* REFERENCES */
 let btnAction = null;
-let gameState = 'start';
-
 let stage     = null;
+let piano     = null;
+let pianoKeys = [];
 
+/* CONTROL */
 let notesToDisplay = [];
-
+let gameState = 'start';
 let problem_answer = null;
-
-
+let problem_answer_num = null;
 
 document.addEventListener('DOMContentLoaded', init);
 
-
+/**
+ * Initializes the page.
+ * When all of DOM elements are loaded, get references, set events, etc.
+ */
 function init() {
     btnAction = document.getElementById('btn-action');
     stage     = document.getElementById('stage');
+    piano     = document.getElementById('piano');
 
     btnAction.addEventListener('click', () => {
         actionButtonBehavior[gameState]();
     });
+
+    createPiano();
 }
 
 /**
@@ -37,6 +43,9 @@ function init() {
  */
 const actionButtonBehavior = {
     start: () => {
+        /* Set problem for the page */
+        // TODO: customization should be possible. Right now, the notes that
+        //       will be selected are only the ones without ledger lines.
         const MAJOR_PATTERN = "wwwhwwh";
         let patternIndex = 0;
 
@@ -46,12 +55,14 @@ const actionButtonBehavior = {
             patternIndex++;
         }
 
-        /* update the stage */
+        let prevProblem = problem_answer_num; // save this reference for piano.
+        /* Update the stage */
         clearStage();
         loadQuestion();
         stage.classList.remove('flex-center');
         gameState = 'question';
         btnAction.innerText = "Show Answer";
+        deselectKey(prevProblem);
     },
 
 
@@ -59,10 +70,12 @@ const actionButtonBehavior = {
         revealAnswer();
         gameState = 'answer';
         btnAction.innerText = "Next Question"
+        selectKey(problem_answer_num);
     },
 
 
     answer: () => {
+        deselectKey(problem_answer_num);
         clearStage();
         loadQuestion();
         gameState = 'question'
@@ -71,28 +84,26 @@ const actionButtonBehavior = {
 }
 
 /**
- * Picks the next note and generates the SVG of the notation. The `stage` container
- * and the `problem_answer` variable is updated.
+ * Picks the next note and generates the SVG of the notation. The `stage` 
+ * container and the `problem_answer` and `problem_answer_num` variable is 
+ * updated.
  */
 function loadQuestion() {
     /* Pick random notes to show */
     let randomIndex = Math.floor(Math.random() * notesToDisplay.length);
-    let randomNote = notesToDisplay[randomIndex];
-    let problem = convertNoteName(randomNote);
-    problem_answer = problem; // need to access the answer when revealing solution.
-    console.log(problem);
+    let randomNote  = notesToDisplay[randomIndex];
+    let problem     = convertNoteName(randomNote);
+
+    problem_answer     = problem; // save answer for other contexts.
+    problem_answer_num = randomNote;
 
     // determine which stave to display this problem on.
-    let clefToDisplay = null;
-    if (randomNote < MIDDLE_C_ABS) {
-        clefToDisplay = 'bass';
-    } else {
-        clefToDisplay = 'treble';
-    }
+    let clefToDisplay = (randomNote < MIDDLE_C_ABS) ? 'bass' : 'treble';
 
     /* Render notation */
     let output = document.createElement('div');
     output.classList.add('fade-in');
+    stage.appendChild(output);
 
     const renderer = new Renderer(output, Renderer.Backends.SVG);
     renderer.resize(100, 200);
@@ -100,16 +111,26 @@ function loadQuestion() {
     const context = renderer.getContext();
 
     // create stave and draw.
-    const staveTreble = new Stave(STAVE_XOFFSET, 0,  STAVE_LENGTH, {fill_style: 'black'});
-          staveTreble  .addClef("treble");
-          staveTreble  .setContext(context).draw();
-    const staveBass   = new Stave(STAVE_XOFFSET, STAVE_BASS_XSTART, STAVE_LENGTH, {fill_style: 'black'});
-          staveBass    .addClef("bass");
-          staveBass    .setContext(context).draw();
+    const staveTreble = new Stave( STAVE_XOFFSET, 
+                                   0,  
+                                   STAVE_LENGTH, 
+                                   {fill_style: 'black'} );
+    staveTreble.addClef("treble");
+    staveTreble.setContext(context).draw();
+    
+    const staveBass = new Stave( STAVE_XOFFSET, 
+                                 STAVE_BASS_XSTART, 
+                                 STAVE_LENGTH, 
+                                 {fill_style: 'black'} );
+    staveBass.addClef("bass");
+    staveBass.setContext(context).draw();
 
-    // create StaveNote.
+    // create StaveNote with our problem.
     const notes = [
-        new StaveNote({keys: [problem],  duration: "1", clef: clefToDisplay, stem: 'down'})
+        new StaveNote({ keys: [problem],  
+                        duration: "1", 
+                        clef: clefToDisplay, 
+                        stem: 'down' })
     ];
 
     // create Voice.
@@ -117,16 +138,17 @@ function loadQuestion() {
     voice.addTickables(notes);
     new Formatter().joinVoices([voice]).format([voice], STAVE_LENGTH);
 
-    // draw everything.
+    // draw voice (notes).
     voice.draw(context, (clefToDisplay == 'treble') ? staveTreble : staveBass);
 
-    /* Move everything to the stage. */
-    stage.appendChild(output);
 }
 
-
+/**
+ * Reveals the answer to the current problem. Specifically, a span with text is
+ * created with `id=answer-lettername` in the stage. Also updates the piano.
+ */
 function revealAnswer() {
-    /* Create element */
+    /* Create span with answer */
     let ans = document.createElement('span');
     ans.innerHTML = problem_answer;
     ans.id = 'answer-lettername';
@@ -135,13 +157,101 @@ function revealAnswer() {
 }
 
 
+function createPiano() {
+    /*
+        w - white key
+        b - regular black key
+        A - left shifted black key
+        B - right shifted black key
+    */
+    let pattern = 'wAwBwwAwbwBw'; // defines pattern starting from C.
+
+    // first black key need not be shifted, create it manually.
+    createKey(['white']); // A/0
+    createKey(['black']); // A#/0
+    createKey(['white']); // B/0
+    // start from C.
+    for (let i = 3; i < 88; i++) {
+        let patternIndex = (i-3) % 12;
+        let currPattern = pattern[patternIndex];
+        switch (currPattern) {
+            case 'w' : createKey(['white']); break;
+            case 'b' : createKey(['black']); break;
+            case 'A' : createKey(['black', 'type-a']); break;
+            case 'B' : createKey(['black', 'type-b']); break;
+        }
+    }
+
+    // TODO: apply effects?
+    applyPianoFancyAnimation();
+}
+
+function applyPianoFancyAnimation() {
+    let delay = 0;
+    let deltaDelay = 10;
+    let index = 0;
+    pianoKeys.forEach(key => {
+        key.classList.add(`key-${index}`);
+        key.innerHTML =
+        `
+        <style>
+            .key-${index} {
+                animation: key-intro-animation 300ms ease 1 ${delay}ms;
+            }
+            @keyframes key-intro-animation {
+                0% {
+                    transform: translateY(20px);
+                }
+                100% {
+                    transform: translateY(0px);
+                }
+            }
+        </style>
+        `;
+        delay += deltaDelay;
+        index++;
+    });
+}
+
+/**
+ * Given list of classes of a key, a key is added to the `piano` and the 
+ * `pianoKeys` reference. 
+ * @param {list} classes 
+ */
+function createKey(classes) {
+    let key = document.createElement('div');
+    classes.forEach(element => {
+        key.classList.add(element);
+    });
+    key.classList.add('key');
+    piano.appendChild(key);
+    pianoKeys.push(key); 
+}
+
+/**
+ * The given key index is deselected on the `piano` element.
+ * @param {number} prevProblem 
+ */
+function deselectKey(keyIndex) {
+    // if previous problem exists, clear that key on the piano.
+    if (keyIndex != null) {
+        pianoKeys[keyIndex].classList.remove('selected');
+    }
+}
+
+
+function selectKey(keyIndex) {
+    if (keyIndex != null) {
+        pianoKeys[keyIndex].classList.add('selected');
+    }
+}
 
 
 
 
 /* UTILITIES */
 /**
- * Clears the div with stage id.
+ * Clears the div with `id="stage"`.
  */
 function clearStage() {
     while (stage.firstChild) {
